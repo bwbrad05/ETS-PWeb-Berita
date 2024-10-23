dcb073e654421a71402e25ea3811e19acd579954
bwbrad05 <142643068+bwbrad05@users.noreply.github.com>
2024-10-23 22:12:41 +0700

    Delete javascript.js

--- javascript.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.back-button a').addEventListener('click', function(event) {
-    event.preventDefault();
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth' 
-    });
-});
